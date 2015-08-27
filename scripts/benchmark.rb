f2ba7d036e6befd4a820439d954711c2f84befbc
Lukas Svoboda <lukas.svoboda@gmail.com>
2015-08-27 10:03:08 +0200

    wip: suggestion benchmark - version 2

--- scripts/benchmark.rb
+++ scripts/benchmark.rb
@@ -4,152 +4,66 @@ require 'benchmark'
 require 'package_provider'
 require 'package_provider/repository'
 
+# --------------------------- CONSTANTS-----------------------------------------
+repo_url = ENV['REPO_URL']
+commit_hash = ENV['COMMIT_HASH']
+repo_local_cache = ENV['REPO_LOCAL_CACHE']
+checkout_mask = ['/**']
+checkout_mask = ENV['REPO_CHECKOUT_MASK'].split(',') if ENV['REPO_CHECKOUT_MASK']
+
+$mutex = Mutex.new
+PackageProvider.logger = Logger.new('log/debug.log')
+
+$dest_dirs = []
+$repos = []
+
+puts 'preparing 10 repos into array'
+
+THREADS = 6
+
+
 def get_temp_dir_name(prefix)
   t = Dir.mktmpdir(prefix)
   FileUtils.rm_rf(t)
   t
 end
 
-puts 'benchmark started'
-
-# --------------------------- CONSTANTS-----------------------------------------
-repo_url = ENV['REPO_URL']
-commit_hash = ENV['COMMIT_HASH']
-repo_local_cache = ENV['REPO_LOCAL_CACHE']
-repo_local_cache_fetched = ENV['REPO_LOCAL_CACHE_FETCHED']
-checkout_mask = ['/**']
-if ENV['REPO_CHECKOUT_MASK']
-  checkout_mask = ENV['REPO_CHECKOUT_MASK'].split(',')
-end
-
-# ------------------------------ INIT  -----------------------------------------
-mutex = Mutex.new
-PackageProvider.logger = Logger.new('debug.log')
-dest_dirs = []
-not_fetched_repos = []
-fetched_repos = []
-
-puts 'preparing 10 repos into array'
-
-Benchmark.bm do |x|
-  x.report('init') do
-    (0..4).map do
-      not_fetched_repos << PackageProvider::Repository.new(
-        repo_url, repo_local_cache)
-
-      fetched_repos << PackageProvider::Repository.new(
-        repo_url, repo_local_cache_fetched) if repo_local_cache_fetched
-    end
+def init_repos(count, repo_url: , repo_local_cache: nil)
+  count.times do |i|
+    $repos << PackageProvider::Repository.new(repo_url, repo_local_cache)
   end
 end
 
-# ----------------------------- TEST not fetched -------------------------------
-
-puts 'Starting benchmark with old repos'
-
-Benchmark.bm do |x|
-  x.report('') do
-    threads = (0..4).map do |i|
-      Thread.new do
-        (0..4).map do |ii|
-          dest_dir = get_temp_dir_name('pp_benchmark_')
-
-          mutex.synchronize do
-            dest_dirs << dest_dir
-          end
-
-          x.report("clone #{ii} for #{i}") do
-            not_fetched_repos[0].clone(
-              dest_dir, commit_hash, checkout_mask, false)
-          end
-        end
-      end
-    end
-    threads.map(&:join)
-  end
-end
-
-puts 'Starting benchmark with single old repo'
-
-Benchmark.bm do |x|
-  single_test_repo = PackageProvider::Repository.new(
-    repo_url, repo_local_cache_fetched)
-
-  x.report('') do
-    t = Thread.new do
-      (0..4).map do |ii|
+def clone(threads, count, commit_hash:, checkout_mask:)
+  threads = (0..(threads-1)).to_a.map do |thread_idx|
+    Thread.new do
+      count.times do |ii|
+        # init dest dir, and remember it for deleting later
         dest_dir = get_temp_dir_name('pp_benchmark_')
+        $mutex.synchronize { $dest_dirs << dest_dir }
 
-        mutex.synchronize do
-          dest_dirs << dest_dir
-        end
-
-        x.report("clone #{ii}") do
-          single_test_repo.clone(
-            dest_dir, commit_hash, checkout_mask, false)
-        end
+        # clone repo
+        $repos[thread_idx].clone( dest_dir, commit_hash, checkout_mask, false)
       end
     end
-    t.join
   end
-  single_test_repo.destroy
+
+  # finish when all threads are done
+  threads.map(&:join)
 end
 
-# ----------------------------- TEST fetched -----------------------------------
-
-if repo_local_cache_fetched
-  puts 'Starting benchmark with fetched repo'
-
+begin
   Benchmark.bm do |x|
-    x.report('') do
-      threads = (0..4).map do |i|
-        Thread.new do
-          (0..4).map do |ii|
-            dest_dir = get_temp_dir_name('pp_benchmark_')
-
-            mutex.synchronize do
-              dest_dirs << dest_dir
-            end
-
-            x.report("clone #{ii} for #{i}") do
-              fetched_repos[i].clone(
-                dest_dir, commit_hash, checkout_mask, false)
-            end
-          end
-        end
-      end
-      threads.map(&:join)
-    end
+    x.report('init') { init_repos(THREADS, repo_url: repo_url, repo_local_cache: repo_local_cache) }
+    x.report('clone-threads-1') { clone(1, 12, commit_hash: commit_hash, checkout_mask: checkout_mask) }
+    x.report('clone-threads-2') { clone(2, 6, commit_hash: commit_hash, checkout_mask: checkout_mask) }
+    x.report('clone-threads-3') { clone(3, 4, commit_hash: commit_hash, checkout_mask: checkout_mask) }
+    x.report('clone-threads-4') { clone(4, 3, commit_hash: commit_hash, checkout_mask: checkout_mask) }
+    #x.report('clone-threads-5') { clone(5, 3, commit_hash: commit_hash, checkout_mask: checkout_mask) }
+    x.report('clone-threads-6') { clone(6, 2, commit_hash: commit_hash, checkout_mask: checkout_mask) }
   end
 
-  puts 'Starting benchmark with single fetched repo'
-
-  Benchmark.bm do |x|
-    single_test_repo = PackageProvider::Repository.new(
-      repo_url, repo_local_cache_fetched)
-
-    x.report('') do
-      t = Thread.new do
-        (0..4).map do |ii|
-          dest_dir = get_temp_dir_name('pp_benchmark_')
-
-          mutex.synchronize do
-            dest_dirs << dest_dir
-          end
-
-          x.report("clone #{ii}") do
-            new_fetched_repos[0].clone(
-              dest_dir, commit_hash, checkout_mask, false)
-          end
-        end
-      end
-      t.join
-    end
-    single_test_repo.destroy
-  end
+ensure
+  $repos.map(&:destroy)
+  $dest_dirs.each { |d| FileUtils.rm_rf(d) }
 end
-# ------------------------------- CLEAR TEST -----------------------------------
-
-not_fetched_repos.map(&:destroy)
-fetched_repos.map(&:destroy)
-dest_dirs.each { |d| FileUtils.rm_rf(d) }
